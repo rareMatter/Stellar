be2a7c6e960a3335b0158e294fde7bf42168c3ac
Jesse Spencer <jesse@nebulist.app>
2021-04-27 14:06:52 -0400

    Create StellarLists project

---
+++ StellarLists/StellarLists/AListView.swift
@@ -0,0 +1,20 @@
+//
+//  AListView.swift
+//  StellarLists
+//
+//  Created by Jesse Spencer on 4/27/21.
+//
+
+import Stellar
+
+struct AList: SView {
+    
+    var content: ViewHierarchyObject {
+        
+    }
+    
+    private
+    var list: SListView {
+        
+    }
+}
